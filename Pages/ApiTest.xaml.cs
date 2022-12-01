90ddf98f80278a68b728f500f284a2005a1ffc4b
park byoung chul <ezcaretech.park.b.c@DESKTOP-CAV95B9>
2022-12-01 12:57:20 +0900

    샘플테스트완료

--- Pages/ApiTest.xaml.cs
+++ Pages/ApiTest.xaml.cs
@@ -12,6 +12,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using my.race.connect;
+using System.Windows.Threading;
+using System.Data;
+using Newtonsoft.Json;
 
 namespace MyRaceWPF.Pages
 {
@@ -25,11 +29,54 @@ namespace MyRaceWPF.Pages
             InitializeComponent();
         }
 
-        private void buttonTest_Click(object sender, RoutedEventArgs e)
+        private async void buttonTest_Click(object sender, RoutedEventArgs e)
         {
+
+            var connect = new my.race.connect.DataBases();
+
+            var result = await connect.SelectApiInformation("RaceResult").ConfigureAwait(false);
+            
+
             gridResult.ItemsSource = null;            
 
-            MessageBox.Show("test");
+            MessageBox.Show(result.UrlAddress);
         }
+
+        private async void buttonCall_Click(object sender, RoutedEventArgs e)
+        {
+
+            var _databases = new my.race.connect.DataBases();
+            var _configuration = new my.race.connect.Configurations();
+            var _dataapi = new my.race.connect.DataAPI();
+
+            var parameters = new List<string>()
+            {
+                "20221113",
+                "20221114"
+            };
+
+            var apiInformation = await _databases.SelectApiInformation("RaceResult").ConfigureAwait(false);
+
+            gridResult.ItemsSource = null;
+
+            if (apiInformation != null)
+            {
+                var _parameters = apiInformation.MakeParameters(parameters);
+                var apiResult = await _dataapi.GetFromAPI<object>(apiInformation.UrlAddress, _parameters).ConfigureAwait(false);
+                var resultJson = JsonConvert.SerializeObject(apiResult);
+
+
+                var resultTable = new DataTable();
+
+                resultTable  = JsonConvert.DeserializeObject<DataTable>(resultJson) ;
+
+                Dispatcher.Invoke(DispatcherPriority.Normal, new Action(delegate
+                {
+                    gridResult.ItemsSource = apiResult;
+                }));
+            }           
+
+        }
+        
     }
 }
